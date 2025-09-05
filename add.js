3b537a8dee8ea817428404b881babf4d2a7ad7f6
AzertyWasTaken <quentin.guillermin@orange.fr>
2025-09-05 18:51:52 +0200

    Add files via upload

---
+++ add.js
@@ -0,0 +1,10 @@
+function print_add(a, b) {
+    let num_a = Number(a);
+    let num_b = Number(b);
+
+    if (a == "" || b == "" || isNaN(num_a) || isNaN(num_b)) {
+        return "Invalid entry. Please enter two numbers."
+    } else {
+        return a + " + " + b + " = " + String(num_a + num_b);
+    }
+};
